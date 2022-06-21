91b655f8ae8aed65997970d6765b60d63f92e20b
Lucas <9322695+g4m3r0@users.noreply.github.com>
2022-06-21 14:03:51 +0200

    Refactor serialization
    Separate serialization function

---
+++ LicenseManager/SerializeHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.Json;
+using LicenseManager.Models;
+
+namespace LicenseManager
+{
+    public class SerializeHelper
+    {
+        private readonly JsonSerializerOptions _serializerOptions;
+
+        public SerializeHelper()
+        {
+            this._serializerOptions = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+        }
+
+        public LicenseModel Deserialize(string jsonString)
+        {
+            return JsonSerializer.Deserialize<LicenseModel>(jsonString, _serializerOptions);
+        }
+    }
+}
