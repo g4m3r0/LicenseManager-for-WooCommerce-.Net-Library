c4b2b713bbda51f180f3f1ba6bf3573e4e6a15a3
Lucas <9322695+g4m3r0@users.noreply.github.com>
2023-04-16 23:02:29 +0200

    Add custom date converter

---
+++ LicenseManager/JsonConverter/CustomDateTimeJsonConverter.cs
@@ -0,0 +1,24 @@
+﻿namespace LicenseManagerClient.Lib.JsonConverter
+{
+    using System;
+    using System.Globalization;
+    using System.Text.Json;
+    using System.Text.Json.Serialization;
+
+    public class CustomDateTimeJsonConverter : JsonConverter<DateTime>
+    {
+        public override DateTime Read(
+            ref Utf8JsonReader reader,
+            Type typeToConvert,
+            JsonSerializerOptions options) =>
+                DateTime.ParseExact(reader.GetString(),
+                    "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture);
+
+        public override void Write(
+            Utf8JsonWriter writer,
+            DateTime dateTimeValue,
+            JsonSerializerOptions options) =>
+                writer.WriteStringValue(dateTimeValue.ToString(
+                    "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture));
+    }
+}
--- LicenseManager/Models/License.cs
+++ LicenseManager/Models/License.cs
@@ -1,23 +1,126 @@
-﻿namespace LicenseManager.Models
+﻿namespace LicenseManagerClient.Lib.Models
 {
     using System;
+    using System.Text.Json.Serialization;
+    using global::LicenseManagerClient.Lib.Enums;
+    using JsonConverter;
 
+    /// <summary>
+    /// Represents a license for a product.
+    /// </summary>
     public class License
     {
+        /// <summary>
+        /// Gets or sets the ID of the license.
+        /// </summary>
+        [JsonPropertyName("id")]
         public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ID of the order associated with the license.
+        /// </summary>
+        [JsonPropertyName("orderId")]
         public int OrderId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ID of the product associated with the license.
+        /// </summary>
+        [JsonPropertyName("productId")]
         public int ProductId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ID of the user associated with the license.
+        /// </summary>
+        [JsonPropertyName("userId")]
         public int UserId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the license key.
+        /// </summary>
+        [JsonPropertyName("licenseKey")]
         public string LicenseKey { get; set; }
-        public DateTime ExpiresAt { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date and time when the license expires.
+        /// </summary>
+        [JsonPropertyName("expiresAt")]
+        [JsonConverter(typeof(CustomDateTimeJsonConverter))]
+        public DateTime? ExpiresAt { get; set; }
+
+        /// <summary>
+        /// Gets or sets the number of days the license is valid for.
+        /// </summary>
+        [JsonPropertyName("validFor")]
         public int ValidFor { get; set; }
+
+        /// <summary>
+        /// Gets or sets the source of the license.
+        /// </summary>
+        [JsonPropertyName("source")]
         public int Source { get; set; }
-        public int Status { get; set; }
+
+        /// <summary>
+        /// Gets or sets the status of the license.
+        /// </summary>
+        public LicenseStatus Status { get; set; }
+
+        [JsonPropertyName("status")]
+        private string RawStatus
+        {
+            get
+            {
+                return this.Status.ToString();
+            }
+
+            set
+            {
+                if (this.RawStatus == value)
+                {
+                    return;
+                }
+
+                this.RawStatus = value;
+                Enum.TryParse<LicenseStatus>(value, out var temp);
+                this.Status = temp;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the number of times the license has been activated.
+        /// </summary>
+        [JsonPropertyName("timesActivated")]
         public int? TimesActivated { get; set; }
+
+        /// <summary>
+        /// Gets or sets the maximum number of times the license can be activated.
+        /// </summary>
+        [JsonPropertyName("timesActivatedMax")]
         public int TimesActivatedMax { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date and time when the license was created.
+        /// </summary>
+        [JsonPropertyName("createdAt")]
+        [JsonConverter(typeof(CustomDateTimeJsonConverter))]
         public DateTime CreatedAt { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ID of the user who created the license.
+        /// </summary>
+        [JsonPropertyName("createdBy")]
         public int CreatedBy { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date and time when the license was last updated.
+        /// </summary>
+        [JsonPropertyName("updatedAt")]
+        [JsonConverter(typeof(CustomDateTimeJsonConverter))]
         public DateTime UpdatedAt { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ID of the user who last updated the license.
+        /// </summary>
+        [JsonPropertyName("updatedBy")]
         public int UpdatedBy { get; set; }
     }
 }
